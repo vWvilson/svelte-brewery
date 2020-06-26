4f1303c4d23cff9745246d78d69d2cc2fe302010
Carla Wilson <carla.wilson@bld-marketing.com>
2020-06-26 11:35:19 -0400

    markers added for each brewery

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,41 @@
 	import Footer from "./Footer.svelte"
 	import Map from './Map.svelte';
 	import MapMarker from './MapMarker.svelte';
+
+
+ import {onMount} from 'svelte'
+import { fade, fly } from 'svelte/transition';
+
+
+const endPoint = "https://api.foursquare.com/v2/venues/explore?";
+const parameters = {
+            client_id:"5ELCQIWOQFDSFHTV5R0YOHE3ZTLY3Z3MZPYZBMJ0QZBJ0TLD",
+            client_secret:"G2PI00PVIA3KONNFCFF50MCYY2L0ROUBGPV1AWV4NKEEMOB2",
+            query:'brewery',
+            near:"Pittsburgh",
+            radius: 10000,
+            v:20181107
+        }
+ 
+let venues = []
+   
+         
+onMount(async function() {
+    const response = await fetch(endPoint + new URLSearchParams(parameters));
+    venues = await response.json();
+    venues= venues.response.groups[0].items;
+});
+         
+            
+let yes="false"
+ 
+const handleCheck = (id) => {
+    venues = venues.filter((venue)=> venue.venue.id != id )
+    console.log(venues)
+    console.log(id)
+   
+ }
+	
 </script>
 
 
@@ -20,7 +55,11 @@
 				<Card />
 			</main>
 				<section class="map-container">
-				<Map lat={40.44} lon={-79.99} zoom={10.5} />
+				<Map lat={40.44} lon={-79.99} zoom={10.5}>
+				{#each venues as venue (venue.venue.id)}
+						<MapMarker lat={venue.venue.location.lat} lon={venue.venue.location.lng} label="{venue.venue.name}"/>
+				{/each}
+					</Map>
 				</section>
 			
 				<Footer />
